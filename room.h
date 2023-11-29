29b39b7bbe58b03de54910cddedf515f50117097
“412140” <412140@bsd48.org>
2023-11-29 03:57:15 +0000

    test - home

---
+++ room.h
@@ -0,0 +1,20 @@
+#ifndef ROOM
+#define ROOM
+
+#include <iostream>
+#include <cstring>
+#include<vector>
+#include <map>
+#include "item.h"
+using namespace std;
+
+class Room
+{
+public:
+  char name[50];
+  char Inp[30];
+  
+
+};
+
+#endif
